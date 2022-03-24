aa8b679154174c318b38cd77e6367746ab4dfa91
Thariq Farsha <mthariqfarsha@gmail.com>
2022-03-24 18:20:42 +0700

    Add schedule module

---
+++ src/modules/schedule/scheduleController.js
@@ -0,0 +1,131 @@
+const helperWrapper = require("../../helpers/wrapper");
+const scheduleModel = require("./scheduleModel");
+
+module.exports = {
+  getAllSchedule: async (req, res) => {
+    try {
+      let { page, limit } = req.query;
+      page = +page;
+      limit = +limit;
+
+      const offset = page * limit - limit;
+      const totalData = await scheduleModel.getCountSchedule();
+      const totalPage = Math.ceil(totalData / limit);
+      const pageInfo = {
+        page,
+        limit,
+        offset,
+        totalPage,
+      };
+
+      const result = await scheduleModel.getAllSchedule(limit, offset);
+
+      return helperWrapper.response(
+        res,
+        200,
+        "Success get data",
+        result,
+        pageInfo
+      );
+    } catch (error) {
+      return helperWrapper.response(res, 400, "Bad request", null);
+    }
+  },
+  getScheduleById: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const result = await scheduleModel.getScheduleById(id);
+
+      if (result.length <= 0) {
+        return helperWrapper.response(
+          res,
+          404,
+          `Data by id ${id} not found`,
+          null
+        );
+      }
+      return helperWrapper.response(res, 200, "Success get data", result);
+    } catch (error) {
+      return helperWrapper.response(res, 400, "Bad request", null);
+    }
+  },
+  createSchedule: async (req, res) => {
+    try {
+      const { movieId, premiere, price, location, dateStart, dateEnd, time } =
+        req.body;
+      const setData = {
+        movieId,
+        premiere,
+        price,
+        location,
+        dateStart,
+        dateEnd,
+        time,
+      };
+      const result = await scheduleModel.createSchedule(setData);
+      return helperWrapper.response(res, 200, "Success create data", result);
+    } catch (error) {
+      return helperWrapper.response(res, 400, "Bad request", null);
+    }
+  },
+  updateSchedule: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const checkId = await scheduleModel.getScheduleById(id);
+
+      if (checkId.length <= 0) {
+        return helperWrapper.response(
+          res,
+          404,
+          `Data by id ${id} not found`,
+          null
+        );
+      }
+
+      const { movieId, premiere, price, location, dateStart, dateEnd, time } =
+        req.body;
+      const setData = {
+        movieId,
+        premiere,
+        price,
+        location,
+        dateStart,
+        dateEnd,
+        time,
+        updatedAt: new Date(Date.now()),
+      };
+
+      // eslint-disable-next-line no-restricted-syntax
+      for (const data in setData) {
+        if (!setData[data]) {
+          delete setData[data];
+        }
+      }
+
+      const result = await scheduleModel.updateSchedule(id, setData);
+      return helperWrapper.response(res, 200, "Success get data", result);
+    } catch (error) {
+      return helperWrapper.response(res, 400, "Bad request", null);
+    }
+  },
+  deleteSchedule: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const checkId = await scheduleModel.getScheduleById(id);
+
+      if (checkId.length <= 0) {
+        return helperWrapper.response(
+          res,
+          404,
+          `Data by id ${id} not found`,
+          null
+        );
+      }
+
+      const result = await scheduleModel.deleteSchedule(id);
+      return helperWrapper.response(res, 200, "Success get data", result);
+    } catch (error) {
+      return helperWrapper.response(res, 400, "Bad request", null);
+    }
+  },
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 
 const Router = express.Router();
 const movieRoutes = require("../modules/movie/movieRoutes");
+const scheduleRoutes = require("../modules/schedule/scheduleRoutes");
 
 Router.use("/movie", movieRoutes);
+Router.use("/schedule", scheduleRoutes);
 
 // Router.get("/hello", (req, res) => {
 //   res.status(200);
