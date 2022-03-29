570a05078fe813af03633106d1de68e5be66aaaa
Thariq Farsha <mthariqfarsha@gmail.com>
2022-03-29 14:57:29 +0700

    Set database name to env variable

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: "tickitix",
+  database: process.env.DB_NAME,
 });
 
 connection.connect((error) => {
