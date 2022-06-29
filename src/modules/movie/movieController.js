934de519c49cc06af51dac274bc03850d0738a3d
Thariq Farsha <mthariqfarsha@gmail.com>
2022-06-30 01:21:43 +0700

    Change push notif imageUrl

--- src/modules/movie/movieController.js
+++ src/modules/movie/movieController.js
@@ -126,7 +126,7 @@ module.exports = {
         notification: {
           title: "New Movie",
           body: `${name} is just released! Grab your ticket now!`,
-          imageUrl: result.data.imagePath,
+          imageUrl: req.file.path,
         },
       });
 
