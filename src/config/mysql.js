f4833bf961883cc7a136cfdbe7a87497bcf3b4b7
Thariq Farsha <mthariqfarsha@gmail.com>
2022-07-19 15:57:48 +0700

    Add config to local db

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -5,6 +5,10 @@ const connection = mysql.createConnection({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
+  // host: "localhost",
+  // user: "root",
+  // password: "",
+  // database: "tickitix",
 });
 
 connection.connect((error) => {
